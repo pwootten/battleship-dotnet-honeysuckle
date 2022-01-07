7986e05e232484a99731a68524943baa45d867cb
Sharon Jefferson-Tetteh <Sharon.Jefferson-Tetteh@abelandcole.co.uk>
2022-01-07 08:41:08 +0000

    Removed commented out code

--- Battleship.Ascii/Program.cs
+++ Battleship.Ascii/Program.cs
@@ -459,26 +459,3 @@ namespace Battleship.Ascii
     }
 
 }
-
-//enemyFleet[0].Positions.Add(new Position { Column = Letters.B, Row = 4 });
-////enemyFleet[0].Positions.Add(new Position { Column = Letters.B, Row = 5 });
-////enemyFleet[0].Positions.Add(new Position { Column = Letters.B, Row = 6 });
-////enemyFleet[0].Positions.Add(new Position { Column = Letters.B, Row = 7 });
-//enemyFleet[0].Positions.Add(new Position { Column = Letters.B, Row = 8 });
-
-//enemyFleet[1].Positions.Add(new Position { Column = Letters.E, Row = 6 });
-////enemyFleet[1].Positions.Add(new Position { Column = Letters.E, Row = 7 });
-////enemyFleet[1].Positions.Add(new Position { Column = Letters.E, Row = 8 });
-//enemyFleet[1].Positions.Add(new Position { Column = Letters.E, Row = 9 });
-
-//enemyFleet[2].Positions.Add(new Position { Column = Letters.A, Row = 3 });
-////enemyFleet[2].Positions.Add(new Position { Column = Letters.B, Row = 3 });
-//enemyFleet[2].Positions.Add(new Position { Column = Letters.C, Row = 3 });
-
-//enemyFleet[3].Positions.Add(new Position { Column = Letters.F, Row = 8 });
-////enemyFleet[3].Positions.Add(new Position { Column = Letters.G, Row = 8 });
-//enemyFleet[3].Positions.Add(new Position { Column = Letters.H, Row = 8 });
-
-//enemyFleet[4].Positions.Add(new Position { Column = Letters.C, Row = 5 });
-//enemyFleet[4].Positions.Add(new Position { Column = Letters.C, Row = 6 });
-
